02c078929d3361da53dc2d5feed838dc7ba2d556
Drixx234 <andrycklaguna2024@gmail.com>
2025-10-01 13:33:04 -0600

    inicio de recu

---
+++ Recupoo/src/main/java/RecuperacionPoo20240136/Recupoo/RecupooApplication.java
@@ -0,0 +1,13 @@
+package RecuperacionPoo20240136.Recupoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecupooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecupooApplication.class, args);
+	}
+
+}
---
+++ Recupoo/src/test/java/RecuperacionPoo20240136/Recupoo/RecupooApplicationTests.java
@@ -0,0 +1,13 @@
+package RecuperacionPoo20240136.Recupoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecupooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
